25fc409e57cbe4ade024ca36cc1f338b76418f72
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-03-27 17:14:54 +0530

    Update UserForm.jsx

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,7 +1,14 @@
 import { Grid } from "@mui/material";
 
 const UserForm = (props) => {
-    <Grid container spacing={2} sx={{backgroundColor:"ffffff",}}>
+    <Grid 
+        container
+        spacing={2} 
+        sx={{
+            backgroundColor:"ffffff",
+
+        }}
+    >
 
     </Grid>
 }
