699ffbc1e55af6c60c56d8dd54597401d7e58b13
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-05-11 21:44:50 +0530

    Update Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import Header from './components/Header/Header'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
 
   return (
     <>
+    <Router>
       <Header/>
+      <Switch>
+        {/*<Route path="/about">
+          <About />
+        </Route>*/}
+      </Switch>
+    </Router>
     </>
   )
 }
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+  
+  const [MobileMenu , setMobileMenu] = useState(false)
+  
   return (
-    <div>Navbar</div>
+    <header className='header'>
+      <div className="container d_flex">
+        <div className="categories d_flex">
+          <span className='fa-solid fa-border-all'></span>
+          <h4>
+            Categories <i className='fa fa-chevron-down'></i>
+          </h4>
+        </div>
+        <div className="navlink">
+          <ul className='nav'>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/pages'>pages</Link>
+            </li>
+            <li>
+              <Link to='/user'>user account</Link>
+            </li>
+            <li>
+              <Link to='/vendor'>vendor account</Link>
+            </li>
+            <li>
+              <Link to='/track'>track my order</Link>
+            </li>
+            <li>
+              <Link to='/contact'>contact</Link>
+            </li>
+          </ul>
+
+          <button className='toggle' onClick={() => setMobileMenu(!MobileMenu)}>
+            {
+              MobileMenu? <i className='fas fa-times close home-bth'></i> :
+              <i className='fas fa-bars open'></i>
+            }
+          </button>
+        </div>
+      </div>
+    </header>
   )
 }
 
