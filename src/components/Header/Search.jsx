c2c11962688980663f6a30261da0c852c561fd44
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-05-12 09:40:26 +0530

    update search.jsx

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 export const Search = () => {
   return (
-    <div>Search</div>
+    <section className='search'>
+      <div className="container c_flex">
+        <div className="logo width">
+          <img src={logo} alt="" className='logo-size'/>
+        </div>
+        <div className="search-box f_flex">
+          <i className='fa fa-search'></i>
+          <input type="text" placeholder='Search and hit enter...' />
+          <span>All Category</span>
+        </div>
+        <div className="icon f_flex width">
+          <i className='fa fa-user icon_circle'></i>
+        </div>
+      </div>
+    </section>
   )
 }
 
